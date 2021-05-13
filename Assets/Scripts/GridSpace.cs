5e92c26c721d39dd10cdd18db7a0c5acdfc1f6e1
kaya <kaya@treenod.com>
2021-05-13 23:26:10 +0900

    기초 게임 플레이

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button; 
+    public Text buttonText; 
+    public string playerSide;
+    
+    public void SetSpace () 
+    { 
+        buttonText.text = playerSide; 
+        button.interactable = false; 
+    }
+}
