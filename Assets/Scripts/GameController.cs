29c1fbb254314b82b7f2cef564ed19799d376331
kaya <kaya@treenod.com>
2021-05-15 09:50:22 +0900

    승리 조건과 차례

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,27 +5,82 @@ using System.Collections;
 public class GameController : MonoBehaviour
 {
     public Text[] buttonList;
+    private string playerSide;
 
-    void Awake () 
+    void Awake()
     {
         SetGameControllerReferenceOnButtons();
+        playerSide = "X";
     }
-    
+
     void SetGameControllerReferenceOnButtons()
     {
         for (int i = 0; i < buttonList.Length; i++)
-        {            
+        {
             buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
         }
     }
-    
-    public string GetPlayerSide () 
+
+    public string GetPlayerSide()
     {
-        return "?";
+        return playerSide;
+    }
+
+    public void EndTurn()
+    {
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+        
+        ChangeSides();
     }
     
-    public void EndTurn () 
-    { 
-        Debug.Log("EndTurn is not implemented!"); 
+    void ChangeSides()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X"; // Note: Capital Letters for "X" and "O"
+    }
+
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
     }
 }
